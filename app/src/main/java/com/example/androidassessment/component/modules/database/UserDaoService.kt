968ec452d420374e1674fd640fb6ad16c2ae8fcc
Tuan Nguyen <anhtuan_nguyen@singaporeair.com.sg>
2021-08-07 22:21:08 +0800

    Update gradle file to include room library

--- app/src/main/java/com/example/androidassessment/component/modules/database/UserDaoService.kt
+++ app/src/main/java/com/example/androidassessment/component/modules/database/UserDaoService.kt
@@ -4,19 +4,24 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import io.reactivex.Observable
 
+@Deprecated("Shouldn't be used directly")
 @Dao
 interface UserDaoService {
 
-  @Query("SELECT * FROM user where username = :username and password = :password")
-  fun getUser(username: String, password: String) : User
+    @Query("SELECT * FROM user")
+    fun getUserTable(): Observable<List<User>>
 
-  @Insert(onConflict = OnConflictStrategy.REPLACE)
-  fun insertUser(user: User)
+    @Query("SELECT * FROM user where username = :username and password = :password")
+    fun getUser(username: String, password: String): Observable<List<User>>
 
-  @Query("DELETE FROM user WHERE username = :username")
-  fun deleteUserByUsername(username: String)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertUser(user: User)
 
-  @Query("DELETE FROM user")
-  fun clearAll()
+    @Query("DELETE FROM user WHERE username = :username")
+    fun deleteUserByUsername(username: String)
+
+    @Query("DELETE FROM user")
+    fun clearAll()
 }
